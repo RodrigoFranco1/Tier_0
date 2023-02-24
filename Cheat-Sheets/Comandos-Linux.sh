dcf81ecf8a23c82b407b8d34073cb6f30e771df8
RodrigoFranco1 <115948997+RodrigoFranco1@users.noreply.github.com>
2023-02-24 12:01:10 -0600

    Create Comandos-Linux.sh

---
+++ Cheat-Sheets/Comandos-Linux.sh
@@ -0,0 +1,76 @@
+Tier 0
+# Comandos (Fundamentos de Linux)
+
+#Formato: Comando | Descripción del comando
+| man <tool> | Despliega el manual de la herramienta especificada.
+| <tool> -h | Despliega la ayuda de la herramienta. 
+| apropos <keyword> | Busca en las páginas de manual una palabra clave determinada.
+| cat | Concatenar e imprimir archivos.
+| whoami | Muestra el nombre de usuario actual.
+| id | Devuelve la identidad de los usuarios.
+| hostname | Establece o imprime el nombre del sistema host actual.
+| uname | Despliega el nombre del sistema operativo.
+| pwd | Muestra el nombre del directorio de trabajo.
+| ifconfig | La utilidad `ifconfig` se utiliza para asignar o ver una dirección a una interfaz de red y/o configurar parámetros de interfaz de red.
+| ip | Ip es una utilidad para mostrar o manipular enrutamiento, dispositivos de red, interfaces y túneles. 
+| netstat | Muestra el estado de la red. 
+| ss | Otra utilidad para investigar sockets. 
+| ps | Muestra el estado del proceso. 
+| quién | Muestra quién está conectado. 
+| env | Imprime el entorno o establece y ejecuta un comando. 
+| lsblk | Lista de dispositivos de bloqueo.  
+| lsusb | Lista de dispositivos USB. 
+| lsof | Lista de archivos abiertos. 
+| lspci | Lista de dispositivos PCI. 
+| sudo | Ejecutar comando como un usuario diferente. 
+| su | La utilidad `su` solicita las credenciales de usuario apropiadas a través de PAM y cambia a ese ID de usuario (el usuario predeterminado es el superusuario).  Luego se ejecuta un shell. 
+| useradd | Crea un nuevo usuario o actualiza la información por defecto del nuevo usuario. 
+| userdel | Elimina una cuenta de usuario y archivos relacionados. 
+| usermod | Modifica una cuenta de usuario. 
+| addgroup | Añade un grupo al sistema. 
+| delgroup | Elimina un grupo del sistema. 
+| passwd | Cambia la contraseña del usuario.
+| dpkg | Instalar, quitar y configurar paquetes basados en Debian. 
+| apt | Utilidad de línea de comandos de gestión de paquetes de alto nivel. 
+| aptitud | Alternativa a `apt`. 
+| snap | Instalar, quitar y configurar paquetes de complemento. 
+| gem | Gestor de paquetes estándar para Ruby. 
+| pip | Gestor de paquetes estándar para Python. 
+| git | Utilidad de línea de comandos del sistema de control de revisiones. 
+| systemctl | Servicio basado en línea de comandos y gestor de control systemd.
+| ps | Imprime una instantánea de los procesos actuales. 
+| journalctl | Consulta la revista systemd. 
+| matar | Envía una señal a un proceso. 
+| bg | Pone un proceso en segundo plano.
+| jobs | Lista todos los procesos que se ejecutan en segundo plano. 
+| fg | Pone un proceso en primer plano. 
+| curl | Utilidad de línea de comandos para transferir datos desde o hacia un servidor. 
+| wget | Una alternativa a `curl` que descarga archivos desde el servidor FTP o HTTP(s). 
+| python3 -m http.server` | Inicia un servidor web Python3 en el puerto TCP 8000. 
+| ls | Lista el contenido del directorio. 
+| cd | Cambia el directorio. 
+| borrar | Borra el terminal. 
+| touch | Crea un archivo vacío. 
+| mkdir | Crea un directorio. 
+| tree | Lista el contenido de un directorio recursivamente. 
+| mv | Mover o renombrar archivos o directorios. 
+| cp | Copiar archivos o directorios. 
+| nano | Editor de texto basado en terminales. 
+| cuál | Devuelve la ruta a un archivo o enlace. 
+| find | Busca archivos en una jerarquía de directorios. 
+| updatedb | Actualiza la base de datos local para los contenidos existentes en el sistema. 
+| locate | Utiliza la base de datos de localización para encontrar contenidos en el sistema. 
+| más | Busca que se utiliza para leer STDOUT o archivos. 
+| menos | Una alternativa a `más` con más características. 
+| cabeza | Imprime las primeras diez líneas de STDOUT o un archivo. 
+| cola | Imprime las últimas diez líneas de STDOUT o un archivo. 
+| sort | Ordena el contenido de STDOUT o un archivo. 
+| grep | Busca resultados específicos que contengan patrones dados. 
+| cortar | Elimina secciones de cada línea de archivos. 
+| tr | Reemplaza ciertos caracteres. 
+| columna | Utilidad basada en línea de comandos que formatea su entrada en varias columnas. 
+| awk | Lenguaje de escaneo y procesamiento de patrones. 
+| sed | Un editor de flujo para filtrar y transformar texto. 
+| wc | Imprime números de línea nueva, palabra y byte para una entrada dada. 
+| chmod | Cambia el permiso de un archivo o directorio. 
+| chown | Cambia el propietario y el grupo de un archivo o directorio.
